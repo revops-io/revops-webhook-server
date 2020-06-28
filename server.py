9125921ea06de86b7f8017db7659a8260d71f199
Adam Ballai <adam@revops.io>
2020-06-28 14:41:07 -0700

    Improve documentation for python3

--- server.py
+++ server.py
@@ -4,21 +4,21 @@ app = Flask(__name__)
 
 @app.route("/deals/activated", methods=['POST'])
 def deal_activated():
-    """ Get flags from deal activation """
-    # Create a response object from the webhook's json
-    response = request.get_json()
+  """ Get flags from deal activation """
+  # Create a response object from the webhook's json
+  response = request.get_json()
 
-    # Get the deal
-    deal = response.get('deal', {})
-    flags = deal.get('feature_flags', [])
+  # Get the deal
+  deal = response.get('deal', {})
+  flags = deal.get('feature_flags', [])
 
-    for flag in flags:
-      name = flag.get('name')
-      enabled = flag.get('enabled', False)
-      if enabled == True:
-        print(f"Enabling FeatureFlag {name}")
+  for flag in flags:
+    name = flag.get('name')
+    enabled = flag.get('enabled', False)
+    if enabled == True:
+      print("Enabling FeatureFlag: {}".format(name))
 
-    return 'received!', 200
+  return 'received!', 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3005)
+  app.run(debug=True, port=3005)
